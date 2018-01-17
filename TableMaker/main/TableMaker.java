843b0af645a36ea21250faf2bbec70a12b60b4fe
FME <MarianEilers@gmx.de>
2018-01-17 15:36:16 +0100

    Final.

--- TableMaker/main/TableMaker.java
+++
@@ -1,9 +0,0 @@
-
-public class TableMaker {
-	
-	public void tableMaker () {
-	FilesOrg file = new FilesOrg("C:\\Users\\FME\\Desktop\\testtext.txt", "C:\\Users\\FME\\Desktop\\lul.txt");
-
-	file.schreiben();
-	}
-}
